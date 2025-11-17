2d7d6effaf40f381551643afa1e0a6c034f3aab9
SamraT-Hacking <60459631+SamraT-Hacking@users.noreply.github.com>
2025-11-17 22:14:59 +0600

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 // /dream-ludo-server/game.js
 const { v4: uuidv4 } = require('uuid');
 
@@ -15,16 +14,16 @@ const MAX_INACTIVE_TURNS = 5; // UPDATED: Changed from 3 to 5
 
 const START_POSITIONS = {
   [PlayerColor.Green]: 1,
-  [PlayerColor.Yellow]: 14,
+  [PlayerColor.Red]: 14,
+  [PlayerColor.Yellow]: 40,
   [PlayerColor.Blue]: 27,
-  [PlayerColor.Red]: 40,
 };
 
 const PRE_HOME_POSITIONS = {
-    [PlayerColor.Green]: 51,
-    [PlayerColor.Yellow]: 12,
-    [PlayerColor.Blue]: 25,
-    [PlayerColor.Red]: 38,
+  [PlayerColor.Green]: 51,
+  [PlayerColor.Red]: 12,
+  [PlayerColor.Yellow]: 38,
+  [PlayerColor.Blue]: 25,
 };
 
 const SAFE_SPOTS = [1, 9, 14, 22, 27, 35, 40, 48];
